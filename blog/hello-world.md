9f481e16ec634a9a65942cc990aaeac9228fc6f2
Chris AN <seldon.an@gmail.com>
2021-12-25 16:17:32 +0800

    feat: basic

--- blog/hello-world.md
+++ blog/hello-world.md
@@ -1,5 +1,5 @@
 ---
-path: /home
+path: /blog
 date: 2021-12-25T07:37:21.464Z
 title: Hello world
 ---
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      Hello world
+      <p><Link to="/blog">View Blog</Link></p>
+      <p><Link to="/account">My Account</Link></p>
+    </div>
+  )
 }
